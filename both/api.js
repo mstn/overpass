978ce44dd9298828f67c673ad57cd9314aeb14c6
mstn <mstn@git.com>
2015-04-27 18:45:36 +0200

    added error log method to logger

--- both/api.js
+++ both/api.js
@@ -6,4 +6,10 @@ Overpass.log = function(msg){
   if ( Overpass.DEBUG ){
     console.log(msg);
   }
-}
+};
+
+Overpass.error = function(msg){
+  if ( Overpass.DEBUG ){
+    console.error(msg);
+  }
+};
